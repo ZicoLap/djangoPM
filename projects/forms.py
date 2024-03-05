574e4eee927621a50a2e88efe5d71fd84664abac
ZicoLap <zico.moh176@gmail.com>
2024-03-05 13:50:20 +0100

    Project managment

--- projects/forms.py
+++ projects/forms.py
@@ -5,9 +5,20 @@ from . import models
 class ProjectCreateForm(forms.ModelForm):
     class Meta:
         model = models.Project
-        fields = ['title', 'description', 'category']
+        fields = ['category', 'title', 'description']
         widgets={
             'category': forms.Select(),
             'title': forms.TextInput(),
             'description': forms.Textarea()
+        }
+
+
+class ProjectUpdateForm(forms.ModelForm):
+    class Meta:
+        model = models.Project
+        fields = ['category', 'status', 'title']
+        widgets = {
+            'category': forms.Select(),
+            'title': forms.TextInput(),
+            'status': forms.Select()
         }
--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
-from projects.models import Project
+from projects.models import Project, Task
 from . import forms
 
 
@@ -16,4 +16,44 @@ class ProjectCreateView(CreateView):
     model = Project
     form_class = forms.ProjectCreateForm
     template_name = 'project/create.html'
+    success_url = reverse_lazy('project_list')
+
+class ProjectUpdateView(UpdateView):
+    model = Project
+    form_class = forms.ProjectUpdateForm
+    template_name = 'project/update.html'
+    success_url = reverse_lazy('project_list')
+
+    def get_success_url(self):
+        return reverse_lazy('project_update', args=[self.object.id])
+
+
+class TaskCreateView(CreateView):
+    model = Task
+    fields = ['project', 'description']
+    http_method_names = ['post']
+    def get_success_url(self):
+        return reverse_lazy('project_update', args=[self.object.project.id])
+
+
+class TaskUpdateView(UpdateView):
+   model = Task
+   fields = ['is_done']
+
+   http_method_names = ['post']
+
+   def get_success_url(self):
+       return reverse_lazy('project_update', args=[self.object.project.id])
+
+
+class TaskDeleteView(DeleteView):
+    model = Task
+
+    def get_success_url(self):
+        return reverse_lazy('project_update', args=[self.object.project.id])
+
+
+class ProjectDeleteView(DeleteView):
+    model = Project
+    template_name = "project/delete.html"
     success_url = reverse_lazy('project_list')
