b257500211fe9872cb4d4f1f3d19a3c22c06c4b8
ZicoLap <zico.moh176@gmail.com>
2024-03-05 11:46:04 +0100

    initiate project models

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.conf.global_settings import AUTH_USER_MODEL
 
 # Create your models here.
+
+class Project_status(models.IntegerChoices):
+    PENDING = 1, 'Pending'
+    COMPLETED = 2, 'Completed'
+    POSTPONED = 3, 'Postponed'
+    CANCELLED = 4, 'Cancelled'
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
+
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    status = models.IntegerField(choices=Project_status.choices, default=Project_status.PENDING)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Task(models.Model):
+    description = models.TextField()
+    is_done = models.BooleanField(default=False)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.description
+
